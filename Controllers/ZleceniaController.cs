bb39c19f640c604825db1c9bb4e9aa88389fcbf5
SKANER <SKANER@SKAN3R>
2024-06-06 23:43:15 +0200

    Changes

--- Controllers/ZleceniaController.cs
+++ Controllers/ZleceniaController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using PCShopProjekt.Data;
 using PCShopProjekt.Models;
 using Microsoft.AspNetCore.Authorization;
+using PCShopProjekt.ViewModels;
 
 namespace PCShopProjekt.Controllers
 {
@@ -82,16 +83,63 @@ namespace PCShopProjekt.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Id_urzadzenia,Id_klienta,Id_technika,Id_uslugi,Id_platnosci,Data_przyjecia,Opis_usterki,Status")] Zlecenia zlecenia)
+        public async Task<IActionResult> Create(ZleceniaCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(zlecenia);
+                // Tworzenie nowego klienta
+                var klient = new Klienci
+                {
+                    Imie = model.Imie,
+                    Nazwisko = model.Nazwisko,
+                    Email = model.Email,
+                    Numer_telefonu = model.Numer_telefonu,
+                    Adres = model.Adres,
+                    Kod_pocztowy = model.Kod_pocztowy
+                };
+                _context.Klienci.Add(klient);
                 await _context.SaveChangesAsync();
+
+                // Tworzenie nowego urządzenia
+                var urzadzenie = new Urzadzenia
+                {
+                    Id_klienta = klient.Id,
+                    Producent = model.Producent,
+                    Nazwa_urzadzenia = model.Nazwa_urzadzenia,
+                    Data_zakupu = DateTime.Now // Przykładowa data zakupu
+                };
+                _context.Urzadzenia.Add(urzadzenie);
+                await _context.SaveChangesAsync();
+
+                // Tworzenie nowej płatności
+                var platnosc = new Platnosci
+                {
+                    Id_klienta = klient.Id,
+                    Status_platnosci = model.Status_platnosci
+                };
+                _context.Platnosci.Add(platnosc);
+                await _context.SaveChangesAsync();
+
+                // Tworzenie nowego zlecenia
+                var zlecenie = new Zlecenia
+                {
+                    Id_urzadzenia = urzadzenie.Id,
+                    Id_klienta = klient.Id,
+                    Id_platnosci = platnosc.Id,
+                    Data_przyjecia = DateTime.Now, // Automatycznie ustawiona obecna data
+                    Opis_usterki = model.Opis_usterki,
+                    Status = "Przyjęto zlecenie"
+                };
+                _context.Zlecenia.Add(zlecenie);
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
-            return View(zlecenia);
+
+            return View(model);
         }
+
+
 
         // GET: Zlecenia/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -152,8 +200,31 @@ namespace PCShopProjekt.Controllers
                 return NotFound();
             }
 
-            var zlecenia = await _context.Zlecenia
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var zlecenia = await (from zlec in _context.Zlecenia
+                                  join kl in _context.Klienci on zlec.Id_klienta equals kl.Id
+                                  join urz in _context.Urzadzenia on zlec.Id_urzadzenia equals urz.Id
+                                  join tech in _context.Technicy on zlec.Id_technika equals tech.Id into techDetails
+                                  from subTech in techDetails.DefaultIfEmpty()
+                                  join plat in _context.Platnosci on zlec.Id_platnosci equals plat.Id
+                                  join usl in _context.Uslugi on zlec.Id_uslugi equals usl.Id into uslDetails
+                                  from subUsl in uslDetails.DefaultIfEmpty()
+                                  where zlec.Id == id
+                                  select new ZleceniaViewModel
+                                  {
+                                      Id = zlec.Id,
+                                      KlientFullName = kl.Imie + " " + kl.Nazwisko,
+                                      KlientId = kl.Id,
+                                      UrzadzenieId = urz.Id,
+                                      UrzadzenieDetails = urz.Producent + " " + urz.Nazwa_urzadzenia,
+                                      TechnikId = subTech != null ? subTech.Id : (int?)null,
+                                      TechnikFullName = subTech != null ? subTech.Imie + " " + subTech.Nazwisko : "Technik nieprzypsiany",
+                                      StatusPlatnosci = plat.Status_platnosci,
+                                      DataPrzyjecia = zlec.Data_przyjecia,
+                                      OpisUsterki = zlec.Opis_usterki,
+                                      NazwaUslugi = subUsl != null ? subUsl.Nazwa_uslugi : "Usługa nieprzypisana",
+                                      Status = zlec.Status
+                                  }).FirstOrDefaultAsync();
+
             if (zlecenia == null)
             {
                 return NotFound();
@@ -161,6 +232,7 @@ namespace PCShopProjekt.Controllers
 
             return View(zlecenia);
         }
+
 
         // POST: Zlecenia/Delete/5
         [HttpPost, ActionName("Delete")]
---
+++ Models/ZleceniaCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace PCShopProjekt.ViewModels
+{
+    public class ZleceniaCreateViewModel
+    {
+        // Dane klienta
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Email { get; set; }
+        public int Numer_telefonu { get; set; }
+        public string Adres { get; set; }
+        public string Kod_pocztowy { get; set; }
+
+        // Dane urządzenia
+        public string Producent { get; set; }
+        public string Nazwa_urzadzenia { get; set; }
+
+        // Dane płatności
+        public string Status_platnosci { get; set; }
+
+        // Dane zlecenia
+        public DateTime Data_przyjecia { get; set; }
+        public string Opis_usterki { get; set; }
+    }
+}
